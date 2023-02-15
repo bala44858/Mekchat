8dcb223aa36ca4bb9a0ae307760e936b18857778
bala44858 <bala44858@gmail.com>
2023-02-15 22:24:26 +0530

    done home and gallery

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2023, 4, 20, 8)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2023, 2)}); // year, month, date, hour
         });
 });		
 
